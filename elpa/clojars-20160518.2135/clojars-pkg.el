28288c5534e11d78b934d2c07b285b327ca50bba
路正遥 <lzy13870@ly.com>
2016-09-02 13:51:37 +0800

    #

---
+++ elpa/clojars-20160518.2135/clojars-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojars" "20160518.2135" "clojars.org search interface" '((request-deferred "0.2.0")) :url "https://github.com/joshuamiller/clojars.el" :keywords '("docs" "help" "tools"))
